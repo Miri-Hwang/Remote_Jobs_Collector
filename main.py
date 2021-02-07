22733bc329cca1923119e76333ca1e9982d6dd82
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-02-07 13:07:13 +0900

    feat : Add Flask and Link Home

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from get_info import collect_info
+from flask import Flask, render_template, request
 
 db = collect_info()
+
+app = Flask("RemoteJobs")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+app.run(host="127.0.0.1")
