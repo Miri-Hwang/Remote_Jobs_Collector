ab170bf40eb2f03f8fcf6886a67f7cf069a4db5c
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-02-07 13:46:02 +0900

    feat : Add Fake Db

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 #db = collect_info('django')
 
 app = Flask("RemoteJobs")
-db = []
+db = {}
 
 
 @app.route("/")
@@ -15,12 +15,13 @@ def home():
 @app.route("/search")
 def search():
     global db
-    if len(db) != 0:
-        db = []
     job = request.args.get('search')
-    db = collect_info(job)
-
-    return render_template("jobs.html", num_jobs=len(db), subject=job)
+    if job in db:
+        pass
+    else:
+        db[job] = collect_info(job)
+    print(db)
+    return render_template("jobs.html", num_jobs=len(db[job]), subject=job)
 
 
 app.run(host="127.0.0.1")
