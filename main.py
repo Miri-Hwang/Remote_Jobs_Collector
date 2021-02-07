823f821ca2b179ea1ee96bd93fd32258a2fa2cfc
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-02-07 14:26:37 +0900

    feat : Download Results in Csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from get_info import collect_info
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
+from exporter import save_to_file
 
 #db = collect_info('django')
 
@@ -16,12 +17,28 @@ def home():
 def search():
     global db
     job = request.args.get('search')
+    job = job.lower()
     if job in db:
         pass
     else:
         db[job] = collect_info(job)
-    print(db)
     return render_template("jobs.html", num_jobs=len(db[job]), subject=job, jobs=db[job])
+
+
+@app.route("/export")
+def export():
+    try:
+        job = request.args.get('search')
+        if not job:
+            raise Exception()
+        job = job.lower()
+        jobs = db[job]
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
 
 
 app.run(host="127.0.0.1")
